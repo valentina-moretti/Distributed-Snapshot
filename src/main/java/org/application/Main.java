35de8b3d282fc281f0f7bbf58d65717f68bf3161
Valentina Moretti <valentina.moretti@mail.polimi.it>
2023-03-04 15:52:13 +0100

    application

--- src/main/java/org/application/Main.java
+++ src/main/java/org/application/Main.java
@@ -1,5 +1,30 @@
 package org.application;
 
+
+import org.project.SnapshotCreator;
+
+import java.io.IOException;
+import java.io.Serializable;
+
 public class Main {
-    
+
+    public static void main(String[] args){
+        Farm farm = new Farm();
+        Animal a = new Animal();
+        SnapshotCreator snapshotCreator = null;
+        try {
+            snapshotCreator = new SnapshotCreator((Serializable) a);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        farm.addAnimal(a);
+        Animal b = new Animal();
+        farm.addAnimal(b);
+        a.addFriend(b);
+        b.addEnemy(a);
+
+
+    }
+
+
 }
