4103130be4029043504e064de12474155974fe1b
FrancioT <francescomaria.tranquillo@mail.polimi.it>
2023-03-25 14:23:22 +0100

    Revert "mv"
    This reverts commit f4269a8e76e741898f3837f7d42aeefc877e226e.

--- src/main/java/org/project/SnapshotCreator.java
+++ src/main/java/org/project/SnapshotCreator.java
@@ -139,11 +139,10 @@ public class SnapshotCreator implements Serializable
      * @return a String identifier of the connection created
      * @throws IOException
      */
-    synchronized public String connect_to(InetAddress address, Integer port) throws IOException
+    synchronized public String connect_to(InetAddress address, Integer p) throws IOException
     {
-        String name = address.toString() + "-" + port;
-        Socket socket = new Socket(address, port);
-        connectionNames.add(name);
+        String name = address.toString();
+        Socket socket = new Socket(address, p);
         ConnectionManager newConnectionM = new ConnectionManager(socket, name, messages);
         connections.add(newConnectionM);
         messages.addClient(name);
